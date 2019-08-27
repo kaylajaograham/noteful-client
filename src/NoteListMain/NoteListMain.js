1cdd0a9de3fd18d1002eccdfe6dc79062951f747
kaylajaograham <kaylajaograham@gmail.com>
2019-08-26 19:23:53 -0700

    added importing of proptypes

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -7,6 +7,7 @@ import CircleButton from '../CircleButton/CircleButton'
 import ApiContext from '../ApiContext'
 import { getNotesForFolder } from '../notes-helpers'
 import './NoteListMain.css'
+import PropTypes from 'prop-types' 
 
 export default class NoteListMain extends React.Component {
   static defaultProps = {
